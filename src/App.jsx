047a15458545aa028ec692120f4b27bbf259ef60
Sreeja Guduguntla <sreejaguduguntla@gmail.com>
2025-11-05 12:48:21 +0530

    Created components folder and an app.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
+
+export default function App(){
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,7 @@
 import { createRoot } from "react-dom/client"
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
+import App from "./App"
 
 const root = createRoot(document.getElementById("root"))
 
-function Page(){
-  return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
-  )
-}
 
-root.render(
-  <Page />
-)
+root.render(<App />)
