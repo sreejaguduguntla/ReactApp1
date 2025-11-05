8280a6ba174c8eb978897a370caa7bb8d1eb6c37
Sreeja Guduguntla <sreejaguduguntla@gmail.com>
2025-11-05 13:13:16 +0530

    Modified Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 export default function Header(){
   return(
     <header className="header"> 
-          <img src="src/assets/react.svg" width="40px" alt="React logo" />
           <nav>
+            <div className="nav-left">
+              <img src="src/assets/react.svg"  alt="React logo" />
+              <span>React Facts</span>
+            </div>
             <ul className="nav-list">
               <li className="nav-list-items">Pricing</li>
               <li className="nav-list-items">About</li>
